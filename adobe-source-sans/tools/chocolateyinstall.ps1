90309ae41da3d4972a994a60fd6edd49089a9bb9
Jesse Stricker <git@jessestricker.de>
2022-11-15 14:30:28 +0100

    feat(adobe-source-sans): add package

---
+++ adobe-source-sans/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$fontFiles = Get-ChildItem -LiteralPath $toolsDir -Filter "*.ttf" | Select-Object -ExpandProperty FullName
+$installCount = Install-ChocolateyFont -Multiple -Paths $fontFiles
+
+if ($installCount -eq 0) {
+    Throw 'All font installation attempts failed!'
+}
+elseif ($installCount -lt $fontFiles.Count) {
+    Write-Warning "$($fontFiles.Count - $installCount) fonts failed to install."
+}
